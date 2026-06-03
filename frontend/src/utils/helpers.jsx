7866002c8c5f51d56d66784d0b6c93505fcd5934
Bhagyesh <bhagyeshbhatt1910@gmail.com>
2026-06-03 12:32:22 +0530

    Added new expense categories with icons and styling

--- frontend/src/utils/helpers.jsx
+++ frontend/src/utils/helpers.jsx
@@ -9,6 +9,15 @@ import {
   BookOutlined,
   QuestionCircleOutlined,
   CoffeeOutlined,
+  RocketOutlined,
+  SafetyCertificateOutlined,
+  RiseOutlined,
+  DollarOutlined,
+  CreditCardOutlined,
+  LaptopOutlined,
+  GiftOutlined,
+  BankOutlined,
+  AlertOutlined,
 } from "@ant-design/icons";
 
 /**
@@ -95,6 +104,83 @@ export const CATEGORY_META = {
     bgColor: "#f0f5ff",
     icon: <BookOutlined />,
   },
+  Travel: {
+  color: "#1677ff",
+  bgColor: "#e6f4ff",
+  icon: <RocketOutlined />,
+},
+
+Insurance: {
+  color: "#13c2c2",
+  bgColor: "#e6fffb",
+  icon: <SafetyCertificateOutlined />,
+},
+
+Investment: {
+  color: "#52c41a",
+  bgColor: "#f6ffed",
+  icon: <RiseOutlined />,
+},
+
+Salary: {
+  color: "#faad14",
+  bgColor: "#fffbe6",
+  icon: <DollarOutlined />,
+},
+
+"Freelance Income": {
+  color: "#73d13d",
+  bgColor: "#fcffe6",
+  icon: <DollarOutlined />,
+},
+
+Subscriptions: {
+  color: "#722ed1",
+  bgColor: "#f9f0ff",
+  icon: <CreditCardOutlined />,
+},
+
+"Gym/Fitness": {
+  color: "#f5222d",
+  bgColor: "#fff1f0",
+  icon: <HeartOutlined />,
+},
+
+Electronics: {
+  color: "#2f54eb",
+  bgColor: "#f0f5ff",
+  icon: <LaptopOutlined />,
+},
+
+"Personal Care": {
+  color: "#eb2f96",
+  bgColor: "#fff0f6",
+  icon: <SmileOutlined />,
+},
+
+Gifts: {
+  color: "#c41d7f",
+  bgColor: "#fff0f6",
+  icon: <GiftOutlined />,
+},
+
+Taxes: {
+  color: "#fa8c16",
+  bgColor: "#fff7e6",
+  icon: <BookOutlined />,
+},
+
+Emergency: {
+  color: "#cf1322",
+  bgColor: "#fff1f0",
+  icon: <AlertOutlined />,
+},
+
+Savings: {
+  color: "#389e0d",
+  bgColor: "#f6ffed",
+  icon: <BankOutlined />,
+},
   Other: {
     color: "#8c8c8c", // Grey
     bgColor: "#f5f5f5",
